502142816ec73bfbd36599a0d8d81a126108c652
iceQHdrop <iceqhdrop@outlook.com>
2021-11-21 17:49:13 +0800

    update mst

--- mst/result/rlt_abundance/plot.py
+++ mst/result/rlt_abundance/plot.py
@@ -5,6 +5,7 @@ from plotnine import*
 from skbio.diversity import beta_diversity
 from skbio.stats.ordination import *
 from scipy.spatial.distance import pdist, squareform
+from sklearn.decomposition import PCA
 
 
 
@@ -19,17 +20,18 @@ if __name__ == '__main__':
 #          xlim(['kindergarten', 'Pupils', 'mid_school', 'youth', 'mid_age', 'elder', 'Unknown'])+
 #          theme_bw())
 #     p.save('./transfer_result/plot.jpg')
+
      # PCOA plot
      metaNormal = pd.read_csv('../../../data/normal-meta.csv', index_col=0)['Group']
-     abundanceNormal = pd.read_csv('../../../data/relative_abundance.csv', index_col=0)[metaNormal.index.tolist()].T
+     abundanceNormal = pd.read_csv('../../../data/relative-abundance.csv', index_col=0)[metaNormal.index.tolist()].T
      # bcDm = beta_diversity('braycurtis', abundanceNormal, abundanceNormal.index)
-     bcDm = squareform(pdist(abundanceNormal, metric='jensenshannon'))
-     bcPcoa = pd.DataFrame(pcoa(bcDm, number_of_dimensions=2).samples.values.tolist(), 
+     jsDm = squareform(pdist(abundanceNormal, metric='jensenshannon'))
+     jsPcoa = pd.DataFrame(pcoa(jsDm, number_of_dimensions=2).samples.values.tolist(), 
                               index=abundanceNormal.index, columns=['PC1', 'PC2'])
      
-     pcoaPlot = (ggplot(bcPcoa, aes('PC1', 'PC2', color=metaNormal))+
+     pcoaPlot = (ggplot(jsPcoa, aes('PC1', 'PC2', color=metaNormal, fill=metaNormal))+
                     geom_point(size=2)+
-                    stat_ellipse()+
+                    stat_ellipse(geom = "polygon", alpha = 0.25)+
                     theme_bw()+
                     theme(axis_line = element_line(color="gray", size = 2))+
                     theme(panel_grid_major = element_blank(), panel_grid_minor = element_blank(), panel_background = element_blank())+
@@ -37,4 +39,21 @@ if __name__ == '__main__':
                     theme(legend_position = (0.8,0.8))+
                     theme(text=element_text(size=20)))
      pcoaPlot.save('pcoa_plot.jpg')
-      
+     
+     # PCA plot
+     pca = PCA(n_components=2)
+     pcaDf = pd.DataFrame(data = pca.fit_transform(abundanceNormal), columns=['PC1', 'PC2'])
+     pcaDf['Env'] = metaNormal.values
+     print(pcaDf)
+     pcaPlot = (ggplot(pcaDf, aes('PC1', 'PC2', color='Env', fill='Env'))+
+                    geom_point(size=2)+
+                    stat_ellipse(geom = "polygon", alpha = 0.25)+
+                    theme_bw()+
+                    theme(axis_line = element_line(color="gray", size = 2))+
+                    theme(panel_grid_major = element_blank(), panel_grid_minor = element_blank(), panel_background = element_blank())+
+                    theme(figure_size=(10, 10))+
+                    theme(legend_position = (0.8,0.8))+
+                    theme(text=element_text(size=20)))
+     pcaPlot.save('pca_plot.jpg')
+     
+     
