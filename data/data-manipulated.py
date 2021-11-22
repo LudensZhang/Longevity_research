80090795caaeb55a6532f5c4a73743ec6a1f92b2
iceQHdrop <iceQHdrop@outlook.com>
2021-11-22 22:19:18 +0800

    update all

--- data/data-manipulated.py
+++ data/data-manipulated.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     rawTaxa = pd.read_csv('raw_data/clean_tax.txt', sep='\t').drop(['Unnamed: 0', 'quality'], axis=1)
     rawTaxa = rawTaxa[['domain', 'phylum', 'class', 'order', 'family', 'genus']]
     taxaName = []
-    
+    # Adapt taxa to expert readable
     for i in rawTaxa.index:
         name = f'k__{if_uc(rawTaxa, i, 0)};p__{if_uc(rawTaxa, i, 1)};c__{if_uc(rawTaxa, i, 2)};o__{if_uc(rawTaxa, i, 3)};f__{if_uc(rawTaxa, i, 4)};g__{if_uc(rawTaxa, i, 5)};s__'
         taxaName.append(name)
@@ -20,13 +20,15 @@ if __name__ == '__main__':
     rawAbundance['Unnamed: 0'] = taxaName
     rawAbundance.rename(columns={'Unnamed: 0': 'Samples'}, inplace=True)
     rawAbundance.to_csv('abundance.csv', index=0)
-    
+    # Create metadata
     rawMeta.index.rename('SampleID', inplace=True)
     normalMeta = rawMeta[rawMeta['Group'] != 'young soldier']
     soldierMeta = rawMeta[rawMeta['Group'] == 'young soldier']
     normalMeta.to_csv('normal-meta.csv')
     soldierMeta.to_csv('soldier-meta.csv')
-    
+    # Create relative abundanceNormal
+    rltAbundance = pd.DataFrame()
+    rltAbundance['Samples'] = rawAbundance['Samples']
     for column in rawAbundance.columns[1:]:
-        rawAbundance[column] = rawAbundance[column].apply(lambda x: x/rawAbundance[column].sum())
-    rawAbundance.to_csv('relative_abundance.csv')
+        rltAbundance[column] = rawAbundance[column].apply(lambda x: x/rawAbundance[column].sum())
+    rltAbundance.to_csv('relative-abundance.csv', index=0)
---
+++ data/update-abundance.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+if __name__ == '__main__':
+    rltAbundance = pd.read_csv('relative-abundance.csv', index_col=0)
+    for column in rltAbundance.columns:
+        rltAbundance[column] = rltAbundance[column].apply(lambda x: x if x > 0.001 else 0)
+    rltAbundance['SUM'] = rltAbundance.apply(lambda x: x.sum(), axis=1)
+    rltAbundance = rltAbundance.sort_values('SUM', ascending=False).iloc[:577].drop('SUM', axis=1)
+    rltAbundance.to_csv('rlt-updated.csv')
+    
+    
+        
