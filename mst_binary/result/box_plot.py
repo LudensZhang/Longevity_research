56601a08b45e8d028511e57e5a4b65316d0880dc
iceQHdrop <iceqhdrop@outlook.com>
2021-12-20 14:06:00 +0800

    update boxplot

---
+++ mst_binary/result/box_plot.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from plotnine import *
+
+
+if __name__ == '__main__':
+    centenarienInde = pd.read_csv('centenarien/independent_result/layer-2.csv')
+    centenarienTrans = pd.read_csv('centenarien/transfer_result/layer-2.csv')
+    elderInde = pd.read_csv('elder/independent_result/layer-2.csv')
+    elderTrans = pd.read_csv('elder/transfer_result/layer-2.csv')
+    centenarienInde = centenarienInde.melt(id_vars='Unnamed: 0')
+    centenarienTrans = centenarienTrans.melt(id_vars='Unnamed: 0')
+    elderInde = elderInde.melt(id_vars='Unnamed: 0')
+    elderTrans = elderTrans.melt(id_vars='Unnamed: 0')
+    centenarienInde['mode'] = 'Independent'
+    centenarienTrans['mode'] = 'Transfer'
+    elderInde['mode'] = 'Independent'
+    elderTrans['mode'] = 'Transfer'
+    centenarienData = centenarienInde.append(centenarienTrans, ignore_index=True)
+    elderData = elderInde.append(elderTrans, ignore_index=True)
+    print(elderData)
+    
+    centenarienBox = (ggplot(centenarienData, aes(x='variable', y='value', fill='mode'))+
+    	    	    	geom_boxplot()+
+			xlab('Env')+
+			ylab('Contibution'))
+    centenarienBox.save('centenarienBox.jpg')
+    
